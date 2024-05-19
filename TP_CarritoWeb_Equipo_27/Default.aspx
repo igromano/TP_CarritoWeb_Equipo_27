d76fe2f1f5f8c76176b0929cc2253e40c22d9b9d
Ignacio Romano <ignacio.romano@swissmedical.com.ar>
2024-05-19 12:27:19 -0300

    Se sube el Home

--- TP_CarritoWeb_Equipo_27/Default.aspx
+++ TP_CarritoWeb_Equipo_27/Default.aspx
@@ -1,6 +1,25 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TP_CarritoWeb_Equipo_27.Default" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>MI ULTIMO INTENTO, NO DOY MAAAAAAAAAS</h1>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <asp:Repeater ID="homeRepeater" runat="server">
+            <ItemTemplate>
+
+                <div class="col">
+                    <div class="card p-2">
+                        <asp:Image ID="imagenArticulo" ImageUrl='<%#Eval("imagenes[0].UrlImagen")%>' runat="server"></asp:Image>
+                        <div class="card-body">
+                            <h5 class="card-title"><%#Eval("art.Nombre") %></h5>
+                            <p class="card-text"><%#Eval("art.Descripcion") %></p>
+                        </div>
+                        <div class="d-flex flex-row-reverse">
+                            <asp:ImageButton ID="addCarrito" OnClick="addCarrito_Click" CommandArgument='<%#Eval("art.id") %>' CommandName="idArticulo" runat="server" CssClass="btn btn-primary" ImageUrl="~/Images/carrito-de-compras.png" />
+                        </div>
+                    </div>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
 </asp:Content>
--- TP_CarritoWeb_Equipo_27/Default.aspx.cs
+++ TP_CarritoWeb_Equipo_27/Default.aspx.cs
@@ -4,14 +4,55 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using negocio;
 
 namespace TP_CarritoWeb_Equipo_27
 {
     public partial class Default : System.Web.UI.Page
     {
+        private ArticuloNegocio artNegocio = new ArticuloNegocio();
+        private ImagenNegocio imgNegocio = new ImagenNegocio();
+        public List<Articulo> listaArticulos;
+        public List<Imagen> listaImagenes;
+        public List<ElementosCarrito> carrito;
+        public ElementosCarrito tmpElemtoCarrito;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (!IsPostBack)
+            {
+                carrito = new List<ElementosCarrito>();
+                
+                Session.Add("listaArticulos", artNegocio.listarArticulos());
+                Session.Add("listaImagenes", imgNegocio.listarImagenes());
+                Session.Add("listaCarrito", carrito);
+                
+                listaImagenes = (List<Imagen>)Session["listaImagenes"];
+                listaArticulos = (List<Articulo>)Session["listaArticulos"];
+
+                foreach(Articulo art  in listaArticulos)
+                {
+                    tmpElemtoCarrito = new ElementosCarrito();
+                    tmpElemtoCarrito.art = art;
+                    tmpElemtoCarrito.imagenes = listaImagenes.FindAll(img => img.IdArticulo == art.Id);
+                    carrito.Add(tmpElemtoCarrito);
+                }
+            }
+
+
+
+            homeRepeater.DataSource = carrito;
+            homeRepeater.DataBind();
+            
         }
+
+        protected void addCarrito_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+
+
     }
 }
